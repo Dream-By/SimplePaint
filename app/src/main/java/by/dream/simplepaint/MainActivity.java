6df8d879d0d26eb53f7336cd5f1e18254ab5f857
dzmitry.dzianisau@gmail.com <Dream-By>
2020-04-07 12:52:20 +0300

    Initial commit

---
+++ app/src/main/java/by/dream/simplepaint/MainActivity.java
@@ -0,0 +1,15 @@
+package by.dream.simplepaint;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        Draw2D draw2D = new Draw2D(this);
+        setContentView(draw2D);
+    }
+}
